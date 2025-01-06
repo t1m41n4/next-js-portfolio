ce6faadd121196994c5cf06a2ca9b2db5c453794
Tim <timmaina64@gmail.com>
2025-01-06 17:40:47 +0300

    add content to home, projects, and contact  pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import Intro from "@/components/intro";
+import RecentProjects from "@/components/recent-projects";
+import {MDXRemote} from 'next-mdx-remote/rsc'
 
 export default function Home() {
   return (
     <section className="py-24">
-      <div className="container">
-        <h1 className="text-3xl font-bold">Portfolio</h1>
+      <div className="container max-w-3xl">
+        <Intro />
+
+        <RecentProjects />
       </div>
     </section>
   )
---
+++ components/intro.tsx
@@ -0,0 +1,28 @@
+
+import Image from 'next/image'
+import authorImage from '@/public/images/authors/tim.JPG'
+
+export default function Intro() {
+  return (
+    <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
+      <div className='mt-2 flex-1 md:mt-0'>
+        <h1 className='title no-underline'>Hey, I&#39;m Tim.</h1>
+        <p className='mt-3 font-light text-muted-foreground'>
+          I&#39;m a software engineer based in Nairobi, Kenya. I&#39;m
+          passionate about learning new technologies and sharing knowledge with
+          others.
+        </p>
+      </div>
+      <div className='relative'>
+        <Image
+          className='flex-1 rounded-lg grayscale'
+          src={authorImage}
+          alt='Tim Maina'
+          width={175}
+          height={175}
+          priority
+        />
+      </div>
+    </section>
+  )
+}
